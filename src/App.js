8c4e43b4dde3958b9e59efa140039cdc0ab5cc99
Muhammed Berisso <muhammedb8826@gmail.com>
2023-04-19 09:06:16 +0300

    Setup routers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       hello
     </div>
   );
