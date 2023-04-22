aa51a2b389dee5a26cad4d095d0e6a85bef7e297
Muhammed Berisso <muhammedb8826@gmail.com>
2023-04-22 16:29:46 +0300

    Fetched data from PI and display

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Home from './components/Home';
 import Country from './components/Country';
-import Details from './components/Details';
+import DetailsItem from './components/DetailsItem';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/country" element={<Country />} />
-        <Route path="/country/:details" element={<Details />} />
+        <Route path="/country/:details" element={<DetailsItem />} />
       </Routes>
     </div>
   );
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function Details() {
+export default function Details({
+  cloud,
+  conditionText,
+  conditionIcon,
+  humidity,
+  lastUpdated,
+  windDegree,
+  tempreture,
+}) {
+  const url = `https:${conditionIcon}`;
   return (
-    <div>
+    <Container>
       <nav>
         <Link to="/">{'<'}</Link>
       </nav>
-      details
-    </div>
+      <ul>
+        <li>{cloud}</li>
+        <li><img src={url} alt="weather icon" /></li>
+        <li>{conditionText}</li>
+        <li>{humidity}</li>
+        <li>{lastUpdated}</li>
+        <li>{windDegree}</li>
+        <li>{tempreture}</li>
+      </ul>
+    </Container>
   );
 }
+
+Details.propTypes = { cloud: PropTypes.number.isRequired };
+Details.propTypes = { conditionText: PropTypes.string.isRequired };
+Details.propTypes = { conditionIcon: PropTypes.string.isRequired };
+Details.propTypes = { humidity: PropTypes.number.isRequired };
+Details.propTypes = { lastUpdated: PropTypes.string.isRequired };
+Details.propTypes = { windDegree: PropTypes.number.isRequired };
+Details.propTypes = { tempreture: PropTypes.number.isRequired };
