96ed748b49796969b6b510d3a28e1f724e15e0b8
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-04-29 09:59:48 +0300

    Removed unnessary files from main

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import Country from './Country';
-
-export default function Home() {
-  return (
-    <div className="home">
-      <Country />
-    </div>
-  );
-}
--- src/redux/API.js
+++
@@ -1,2 +0,0 @@
-export const APIkey = '593c557b7df54b688db22828231904';
-export const baseURL = 'https://api.weatherapi.com/v1/current.json?key=593c557b7df54b688db22828231904&q=Ethiopia&aqi=no';
